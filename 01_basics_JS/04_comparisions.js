8840293be5c400b80b23c0bb53db0761f9825e06
Yash Vardhan Yadav <yashvardhanrao8@gmail.com>
2026-06-11 17:03:43 +0000

    comparisions

---
+++ 01_basics_JS/04_comparisions.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log(2 > "1"); // Jab aap kisi string aur number ke beech mein relational operators (jaise >, <, >=, <=) use karte hain, toh JavaScript comparison karne se pehle string ko automatically number mein convert kar deta hai.
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);  // null auto bn jata hai 0
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);  // all are false 
+
+// === 
+
+// console.log("2" === 2); // Yeh check karta hai ki kya do values aur unke data types dono bilkul same hain ya nahi.
